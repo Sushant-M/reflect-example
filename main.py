6743cf3ddcfdec6f51a55448cc93d7a599a731dd
Sushant Mathur <sushant.post@gmail.com>
2017-02-21 03:09:50 +0530

    added comments

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import glob
 
 if __name__ == '__main__':
+    #directory where the images are stored
     os.chdir("test_images")
+    #iterate over files with the .png extension
     for file in glob.glob("*.png"):
+        #.delay is used to send the function for processing
+        #on celery
         cowrks_task.delay(file)
--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from celery import Celery
 import requests
 
+#creating an instance, broker is the rabbitmq server
+#running on localhost
 app = Celery('tasks', broker='amqp://localhost//')
 
 
@@ -8,14 +10,26 @@ app = Celery('tasks', broker='amqp://localhost//')
 def cowrks_task(image):
 
 	try:
+		#base url
 		url = 'http://54.145.171.212:8000/pipeline/full'
+
+		#path to the image file
 		path = 'test_images/' + str(image)
+
+		#open the file in byte mode
 		data = open(path, 'rb').read()
+
+		#content headers
 		headers = {'Content-Type': 'image/png'}
+
+		#send the post request and get the response back
 		response = requests.post(url, data=data, headers=headers)
+
+		#convert to text form for printing, can be printed directly as well
 		ret_val = response.text
 		print (ret_val)
 		return ret_val
 	except requests.exceptions.RequestException as err:
+		#if we couldn't get the result for whatever reason, print the error
 		print (err, 'Could not retrieve data')
 		return 'error'
